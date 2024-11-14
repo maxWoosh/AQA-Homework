0e540c65d757d1daa11d805c18111b370051d6e0
maxWoosh <140332381+maxWoosh@users.noreply.github.com>
2024-11-14 09:21:27 +0300

    Lesson_6

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,56 @@
 public class Main {
     public static void main(String[] args) {
+        String[][] array = {
+                {"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"}
+        };
 
+        try {
+            int result = processArray(array);
+            System.out.println("Сумма элементов: " + result);
+        } catch (MyArraySizeException e) {
+            System.out.println("Ошибка размера массива: " + e.getMessage());
+        } catch (MyArrayDataException e) {
+            System.out.println("Ошибка данных в ячейке: " + e.getMessage());
+        }
+    }
+
+    public static int processArray(String[][] array) throws MyArraySizeException, MyArrayDataException {
+        if (array.length != 4) {
+            throw new MyArraySizeException("Массив должен быть размером 4x4");
+        }
+        for (String[] row : array) {
+            if (row.length != 4) {
+                throw new MyArraySizeException("Массив должен быть размером 4x4");
+            }
+        }
+
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                try {
+                    sum += Integer.parseInt(array[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("Некорректные данные в ячейке [" + i + "][" + j + "]: " + array[i][j]);
+                }
+            }
+        }
+        return sum;
     }
 
 }
+
+class MyArraySizeException extends Exception {
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+}
+
+class MyArrayDataException extends Exception {
+    public MyArrayDataException(String message) {
+        super(message);
+    }
+}
+
